727f64f62da262fb13f99ea4866255c72de7f6e5
Swaraj Phalke <swarajphalke425@gmail.com>
2025-02-24 16:17:34 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -8,13 +8,7 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors(
-    {
-        origin: ["https://calculator-frontend-woad.vercel.app"],
-        methods: ["GET", "POST"], 
-        credentials: true
-    }
-));
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", calculatorRoutes);
